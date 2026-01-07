f7595af61885cfd58abf32694955fc0e666256ad
SoYoungLEE-me <lsy000626@gmail.com>
2026-01-08 04:38:27 +0900

    feat:검색 결과 없을 때 ui 추가

--- src/pages/Search/SearchResultPage/SearchResultPage.tsx
+++ src/pages/Search/SearchResultPage/SearchResultPage.tsx
@@ -2,7 +2,7 @@ import useSearchItems from "../../../hooks/useSearchItems";
 import { SEARCH_TYPE } from "../../../models/search";
 import { useSearchParams } from "react-router";
 import LoadingSpinner from "../../../common/components/LoadingSpinner";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import ArtistsSection from "./components/ArtistsSection";
 import TopResult from "./components/TopResult";
 import SongsSection from "./components/SongsSection";
@@ -32,6 +32,9 @@ const SearchResultPage = () => {
   const artists = page?.artists?.items ?? [];
   const albums = page?.albums?.items ?? [];
 
+  const hasNoResults =
+    tracks.length === 0 && artists.length === 0 && albums.length === 0;
+
   return (
     <div>
       <Grid container spacing={3} px={3}>
@@ -47,6 +50,29 @@ const SearchResultPage = () => {
       </Grid>
       <ArtistsSection artists={artists} />
       <AlbumsSection albums={albums} />
+
+      {hasNoResults && (
+        <Box
+          height="60vh"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+        >
+          <Typography variant="h5" sx={{ fontWeight: 700, mb: 1 }}>
+            No results found
+          </Typography>
+
+          <Typography variant="body2" color="text.secondary">
+            We couldn’t find anything for
+          </Typography>
+
+          <Typography variant="body2" sx={{ fontWeight: 600, mt: 0.5 }}>
+            “{query}”
+          </Typography>
+        </Box>
+      )}
     </div>
   );
 };
